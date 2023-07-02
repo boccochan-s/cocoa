e3d54969ed155034fc3693c6d2bbee3ade3ad067
boccochan_s <boccochan.s@gmail.com>
2023-07-02 15:51:19 +0900

    add line reply function

--- src/server.js
+++ src/server.js
@@ -3,16 +3,49 @@
 require('dotenv').config();
 const express = require('express');
 const line = require('@line/bot-sdk');
-const env = process.env;
-
-const HOST = '0.0.0.0'
-const PORT = '3000'
 
 const config = {
-  channelAccessToken: env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: env.CHANNEL_SECRET
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET
 };
 
 const app = express();
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+});
 
-app.listen(PORT, HOST);
+const client = new line.Client(config);
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    Promiss.resolve(null);
+  }
+
+  const recipeURL = `にゃー$、「${event.message.text}」!!\nそれならこんなレシピはどうかにゃ？\nhttps://cookpad.com/search/${event.message.text}`;
+
+  const replyDetail = {
+    type: 'text',
+    text: recipeURL,
+    emojis: getRandomCatEmoji()
+  };
+  console.log('replyDetail: ' + JSON.stringify(replyDetail));
+
+  return client.replyMessage(event.replyToken, replyDetail);
+}
+
+function getRandomCatEmoji() {
+  const eID = (Math.floor(Math.random() * 10) + 1).toString().padStart(3, '0');
+
+  const emojis = [{
+    index: 3,
+    productId: '5ac21184040ab15980c9b43a',
+    emojiId: eID
+  }];
+  console.log('emoji: ' + JSON.stringify(emojis));
+
+  return emojis;
+}
+
+app.listen(process.env.PORT || 3000);
+console.log(`The server is running. The port is at ${process.env.PORT}!`);
